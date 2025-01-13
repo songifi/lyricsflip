57fead93d8af8b5f359ce5fec9235f03b4d8aad3
xaxxoo <bnahmad83@gmail.com>
2025-01-14 00:36:04 +0100

    auth and user services created

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserService } from './providers/user.service';
 
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
