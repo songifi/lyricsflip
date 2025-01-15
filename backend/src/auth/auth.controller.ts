295f1bcc12d9ba9fff87b4c7185b52d1b02d1592
xaxxoo <bnahmad83@gmail.com>
2025-01-15 08:23:57 +0100

    Add class-validator dependency and implement sign-in endpoint in AuthController

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
+import { SignInDto } from './dtos/signIn.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,4 +8,7 @@ export class AuthController {
     // Injecting AuthService
     private readonly authService: AuthService,
   ) {}
+
+  @Post('sign-in')
+  public async signIn(@Body() signInDto: SignInDto) {}
 }
