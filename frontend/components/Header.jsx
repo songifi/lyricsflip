73ab66f5528e012b7b0c90bce970052f2928479c
sirmee7 <sirmee7@gmail.com>
2025-01-21 13:50:36 +0000

    fix: comment the connect wallet

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -33,8 +33,7 @@ const Header = () => {
       <header className="">
         <nav
           aria-label="Global"
-          className="flex items-center justify-between p-3 lg:px-8"
-        >
+          className="flex items-center justify-between p-3 lg:px-8">
           <div className="flex lg:flex-1">
             <Link href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">LyricsFlip</span>
@@ -51,8 +50,7 @@ const Header = () => {
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
               <span className="sr-only">Open main menu</span>
               <MdOutlineMenu aria-hidden="true" className="size-6" />
             </button>
@@ -63,26 +61,24 @@ const Header = () => {
                 key={item.name}
                 href={item.href}
                 onClick={(e) => handleScroll(e, item.isScroll)}
-                className="text-sm/6 font-semibold text-white"
-              >
+                className="text-sm/6 font-semibold text-white">
                 {item.name}
               </a>
             ))}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <Link
               href="#"
               className="text-sm/6 font-semibold text-[#490878] px-3 py-1.5 text-center rounded-lg bg-[#70E3C7]"
             >
               Connect Wallet
             </Link>
-          </div>
+          </div> */}
         </nav>
         <Dialog
           open={mobileMenuOpen}
           onClose={setMobileMenuOpen}
-          className="lg:hidden"
-        >
+          className="lg:hidden">
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
@@ -97,8 +93,7 @@ const Header = () => {
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
+                className="-m-2.5 rounded-md p-2.5 text-gray-700">
                 <span className="sr-only">Close menu</span>
                 <IoMdClose aria-hidden="true" className="size-6" />
               </button>
@@ -110,8 +105,7 @@ const Header = () => {
                     <Link
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                    >
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                       {item.name}
                     </Link>
                   ))}
@@ -119,8 +113,7 @@ const Header = () => {
                 <div className="py-6">
                   <Link
                     href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                     Log in
                   </Link>
                 </div>
