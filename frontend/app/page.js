235f60d4b88cc6b1df2874a904fb5e352c52e3c7
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2025-01-23 14:04:16 +0100

    feat:New feature - update responsiveness - edit color code and text - implemented id for smooth scrolling implementation.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="bg-white">
       <HeroSection />
-      <Game />
       <HowItWorks />
+      <Game />
     </div>
   );
 }
--- frontend/components/HowToPlay.jsx
+++ frontend/components/HowToPlay.jsx
@@ -5,55 +5,57 @@ import React from "react";
 const steps = [
   {
     title: "Connect Wallet",
-    text: "Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class ",
+    text: "Securely connect your wallet to start the game. Don’t have one? No worries, we’ll guide you through the setup.",
   },
   {
-    title: "Connect Wallet",
-    text: "Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class ",
+    title: "Enter Username",
+    text: "Choose a fun and unique username to showcase your identity. This will represent you in challenges and leaderboards. ",
   },
   {
-    title: "Connect Wallet",
-    text: "Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class ",
+    title: "Choose Category & Play",
+    text: "Pick your favourite category and dive into the game! Test your knowledge, beat challenges, and climb the ranks.",
   },
 ];
 
 const HowItWorks = () => {
   return (
-    <section className="bg-purple-900 px-4 py-16 md:py-24 h-screen">
-      <div className="mx-auto max-w-7xl text-center md:my-28">
+    <section
+      id="howItWorks"
+      className="bg-[#490878] px-4 md:py- py-12 md:h-screen"
+    >
+      <div className="mx-auto max-w-7xl text-center border-red-500 py-[100px]">
         {/* Header Section */}
-        <div className="mb-16">
-          <h2 className="mb-4 text-5xl font-bold text-white md:text-5xl lg:text-6xl">
+        <div className="">
+          <h2 className="pb-4 text-5xl font-semibold text-[#FFFFFF] md:text-5xl lg:text-6xl">
             How it works
           </h2>
-          <p className="text-lg text-white/90 md:text-xl">
-            lorem ipsum forem sit amet adipistyd humat thyu lemama strova loci
-            😊
+          <p className="text-lg text-[#FFFFFF] md:text-xl">
+            Have fun testing your lyrical knowledge in three (3) easy steps 😊
           </p>
         </div>
 
         {/* Steps Grid */}
-        <div className="grid gap-8 md:grid-cols-3">
-          {steps.map((step) => (
-            <div className="space-y-4">
+        <div className="grid gap-8 md:grid-cols-3 my-16">
+          {steps.map((step, index) => (
+            <div className="space-y-4 my-10">
               <h3
-                key={step.title}
-                className="text-2xl font-bold text-white md:text-3xl"
+                key={index}
+                className="text-2xl font-bold text-[#FFFFFF] lg:text-3xl pb-3"
               >
                 {step.title}
               </h3>
-              <p className="mx-auto max-w-sm text-white/80"></p>
+              <p className="mx-auto max-w-sm text-[#FFFFFF]"></p>
               <p>{step.text}</p>
             </div>
           ))}
         </div>
 
         {/* Buttons */}
-        <div className="mt-12 flex flex-col items-center justify-center gap-4 md:flex-row">
-          <button className="w-full rounded-full bg-[#70E3C7] px-14 py-4 font-semibold text-gray-900 transition md:w-auto">
+        <div className=" flex flex-col items-center justify-center gap-4 md:flex-row ">
+          <button className="w-full rounded-full bg-[#70E3C7] px-[69px] py-6 font-semibold text-[#090909] transition md:w-auto">
             Play Game
           </button>
-          <button className="w-full rounded-full border-2 border-[#70E3C7] px-8 py-4 font-semibold text-[#70E3C7] md:w-auto">
+          <button className="w-full rounded-full border-2 border-[#70E3C7] px-[51px] py-6  font-semibold text-[#70E3C7] md:w-auto">
             Connect Wallet
           </button>
         </div>
