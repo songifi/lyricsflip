eec3f7d3c29c1d706f898b120961fe3731774cb6
olowo <olowodarey8@gmail.com>
2025-01-26 18:27:04 +0100

    created contributors dashboard

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import Game from "@/components/game/GameSection";
+import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div className="bg-white">
+      <Header />
       <HeroSection />
       <Game />
     </div>
