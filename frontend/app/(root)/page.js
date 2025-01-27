4160bc53a3c88b30c2ce57e24f8663612ce35515
OthmanImam <imkargi@gmail.com>
2025-01-27 08:53:24 -0800

    feat: Implement Sign-Up functionality
    Reconfigured app directory and created auth directory with sign-up path

--- frontend/app/(root)/page.js
+++ frontend/app/(root)/page.js
@@ -1,11 +1,16 @@
+
+import Footer from "@/components/Footer";
 import Game from "@/components/game/GameSection";
 import HeroSection from "@/components/HeroSection";
+import HowItWorks from "@/components/HowToPlay";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <HeroSection />
+      <HowItWorks />
       <Game />
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/components/HowToPlay.jsx
@@ -0,0 +1,86 @@
+"use client";
+import { title } from "process";
+import { useState } from "react";
+import { Modal } from "./ui/modal";
+import { GameSetupForm } from "./modal/GameSetupForm";
+
+const steps = [
+  {
+    title: "Connect Wallet",
+    text: "Securely connect your wallet to start the game. Don’t have one? No worries, we’ll guide you through the setup.",
+  },
+  {
+    title: "Enter Username",
+    text: "Choose a fun and unique username to showcase your identity. This will represent you in challenges and leaderboards. ",
+  },
+  {
+    title: "Choose Category & Play",
+    text: "Pick your favourite category and dive into the game! Test your knowledge, beat challenges, and climb the ranks.",
+  },
+];
+
+const HowItWorks = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleStartGame = () => {
+    console.log("Starting game...");
+    setIsModalOpen(false);
+  };
+
+  return (
+    <section
+      id="howItWorks"
+      className="bg-[#490878] px-4 md:py- py-12 md:h-screen"
+    >
+      <div className="mx-auto max-w-7xl text-center border-red-500 py-[100px]">
+        {/* Header Section */}
+        <div className="">
+          <h2 className="pb-4 text-5xl font-semibold text-[#FFFFFF] md:text-5xl lg:text-6xl">
+            How it works
+          </h2>
+          <p className="text-lg text-[#FFFFFF] md:text-xl">
+            Have fun testing your lyrical knowledge in three (3) easy steps 😊
+          </p>
+        </div>
+
+        {/* Steps Grid */}
+        <div className="grid gap-8 md:grid-cols-3 my-16">
+          {steps.map((step, index) => (
+            <div className="space-y-4 my-10" key={index}>
+              <h3
+                key={index}
+                className="text-2xl font-bold text-[#FFFFFF] lg:text-3xl pb-3"
+              >
+                {step.title}
+              </h3>
+              <p className="mx-auto max-w-sm text-[#FFFFFF]"></p>
+              <p>{step.text}</p>
+            </div>
+          ))}
+        </div>
+
+        {/* Buttons */}
+        <div className=" flex flex-col items-center justify-center gap-4 md:flex-row ">
+          <button
+            className="w-full rounded-full bg-[#70E3C7] px-[69px] py-6 font-semibold text-[#090909] transition md:w-auto"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Play Game
+          </button>
+          <Modal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            title="Guess the song"
+          >
+            <GameSetupForm onStart={handleStartGame} />
+          </Modal>
+          <button className="w-full rounded-full border-2 border-[#70E3C7] px-[51px] py-6  font-semibold text-[#70E3C7] md:w-auto">
+            Connect Wallet
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
