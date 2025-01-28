dd2c27ec6f839e590bd1d132b52800dd406a3f6e
OthmanImam <imkargi@gmail.com>
2025-01-28 02:37:03 -0800

    updated commit

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
 import Header from "@/components/Header";
 import { StarknetProvider } from "@/components/starknet-provider";
 
