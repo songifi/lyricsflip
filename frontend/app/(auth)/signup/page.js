dd9ffad50304042b7675b0284081eb6fefc7ea49
OthmanImam <imkargi@gmail.com>
2025-01-28 01:50:56 -0800

    updat commit

--- frontend/app/(auth)/signup/page.js
+++ frontend/app/(auth)/signup/page.js
@@ -2,9 +2,7 @@ import Signup from "@/components/auth/Sign-up"
 
 export default function SignUpPage() {
   return (
-    // <div className="h-screen w-full flex items-center justify-center bg-#F6EDFC">
         <Signup />
-      // </div>
   )
 }
 
