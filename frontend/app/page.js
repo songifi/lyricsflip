7d19639c33caf3991f4213e6320f38b3f70b85b2
ahmad <abdulkareemahmad413@gmail.com>
2025-01-28 12:27:14 +0100

    finished the log in page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,13 +1,16 @@
+import LogIn from "@/components/auth/LogIn";
 import Game from "@/components/game/GameSection";
 import HeroSection from "@/components/HeroSection";
 import HowItWorks from "@/components/HowToPlay";
 
+
 export default function Home() {
   return (
     <div className="bg-white">
-      <HeroSection />
+      {/* <HeroSection />
       <HowItWorks />
-      <Game />
+      <Game /> */}
+      <LogIn/>
     </div>
   );
 }
