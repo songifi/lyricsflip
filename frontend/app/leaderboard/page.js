d060bf8c4d397e50811d0944b7f4dc49f7482c18
PeterOche <119618121+PeterOche@users.noreply.github.com>
2025-02-22 14:32:18 +0100

    Update leaderboard page.js

--- frontend/app/leaderboard/page.js
+++ frontend/app/leaderboard/page.js
@@ -1,6 +1,7 @@
 import LeaderBoard from "@/components/LeaderBoard";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Game from "@/components/game/GameSection";
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <div className="p-20">
                 <LeaderBoard />
             </div>
+            <Game/>
             <Footer />
         </div>
     );
