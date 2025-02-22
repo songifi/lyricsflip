d50b1adae8e092d6bdb3f8d2e64e7f5317863825
Prabal Kumar Das <prabalkumardas@Prabals-MacBook-Air.local>
2025-02-22 18:37:38 +0530

    Removed unused imports in LeaderBoard

--- frontend/components/LeaderBoard.jsx
+++ frontend/components/LeaderBoard.jsx
@@ -6,8 +6,6 @@ import rankThree from "../public/leaderboard-img/rankThree.svg"
 import avatar from "../public/leaderboard-img/avatar.svg";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import { IoMdArrowDropdown } from "react-icons/io";
-import HomeButton from "./ui/HomeButton"; 
-import PlayAgainButton from "./ui/PlayAgainButton"
 import { Modal } from "./ui/modal";
 import { GameSetupForm } from "./modal/GameSetupForm";
 import { useState } from "react";
