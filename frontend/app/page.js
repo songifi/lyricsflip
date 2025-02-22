f3318e8098424f6c8f6e079c4948cf33a03618dc
Bigcedar <godwinekoh@gmail.com>
2025-02-22 23:35:21 +0100

    feat: update header and hero section design based on new Figma layout (#237)

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,9 @@
-
 import Footer from "@/components/Footer";
 import Game from "@/components/game/GameSection";
 import HeroSection from "@/components/HeroSection";
 import HowItWorks from "@/components/HowToPlay";
 import LeaderBoard from "@/components/LeaderBoard";
 import Header from "@/components/Header";
-
 
 export default function Home() {
   return (
